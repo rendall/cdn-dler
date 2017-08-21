5c4e3bcc193e342fc89a515aca993b622fdd8467
Rendall <rendall@gmail.com>
2017-08-21 16:33:03 +0300

    Still a WIP, but minimum viable. It now parses an HTML file's <SCRIPT> tag and downloads all SRC URL.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 "use strict";
 var R = require('ramda');
 var fs = require('fs');
+var http = require('http');
+var urlParser = require('url');
+//TODO: take this out, put it in a config file.
+var DOWNLOAD_DIR = './js/';
 var openHtmlFile = function (file) { return new Promise(function (resolve, reject) {
     var onResponse = function (err, data) { return err ? onError(err) : onSuccess(data); };
     var onError = function (error) { return reject(error); };
@@ -12,14 +16,54 @@ var openHtmlFile = function (file) { return new Promise(function (resolve, rejec
         onError(error);
     }
 }); };
-var scriptPtrn = /<script.*?src=["']((?:https?:\/\/)?[a-z0-9\/\.\-]+\.[a-z]{2,4}(?![a-z])(?:\/[a-z\/\.\?=0-9\&_]*)?)["']/;
+var scriptPtrn = /(<script.*?src=["']((?:https?:)?\/\/[a-z0-9\/\.\-]+\.[a-z]{2,4}(?![a-z])(?:\/[a-z\-\/\.\?=0-9\&_]*)?)["'].*>)/g;
 var getTag = function (html) {
     var pattern = RegExp(scriptPtrn);
     var exec = pattern.exec(html);
     return (exec != null) ? exec[1] : null;
 };
-var parseFile = function (file) { return openHtmlFile(file).then(function (data) { return getTag(data); }).catch(function (e) { return console.error(e); }); };
+var getAllSrcs = function (html, idx, matches) {
+    if (idx === void 0) { idx = 0; }
+    if (matches === void 0) { matches = []; }
+    var match = RegExp(scriptPtrn).exec(html);
+    if (match == null)
+        return matches;
+    return getAllSrcs(html, match.index, R.append(match, matches));
+};
+var downloadFile = function (exec) { return new Promise(function (resolve, reject) {
+    var url = exec[2];
+    var src = url.startsWith("//") ? "http:" + url : url.startsWith("https:") ? url.replace("https:", "http:") : url;
+    var file_name = urlParser.parse(src).pathname.split('/').pop();
+    var file = fs.createWriteStream(DOWNLOAD_DIR + file_name);
+    try {
+        http.get(src, function (res) {
+            res.on('data', function (data) {
+                file.write(data);
+            }).on('end', function () {
+                file.end();
+                console.log(file_name + ' downloaded to ' + DOWNLOAD_DIR);
+                resolve(exec);
+            });
+        });
+    }
+    catch (error) {
+        reject(error);
+    }
+}); };
+var downloadFiles = function (matches) {
+    var promises = matches.map(downloadFile);
+    Promise.all(promises).then(function (x) { return console.log("all promise:", x); }).catch(function (e) { return console.log("all error:", e); });
+    setTimeout(function () {
+        promises.forEach(function (p) { return console.log(p); });
+    }, 1000);
+    return matches;
+};
+var parseFile = function (file) { return openHtmlFile(file)
+    .then(getAllSrcs)
+    .then(downloadFiles)
+    .catch(function (e) { return console.error(e); }); };
 exports.openHtmlFile = openHtmlFile;
 exports.getTag = getTag;
 exports.parseFile = parseFile;
+exports.getAllSrcs = getAllSrcs;
 require('make-runnable');
--- ts/index.ts
+++ ts/index.ts
@@ -1,30 +1,81 @@
+
 const R = require('ramda');
 const fs = require('fs');
+const http = require('http');
+const urlParser = require('url');
 
-const openHtmlFile = (file: string):Promise => new Promise((resolve: any, reject: any) => {
-    const onResponse = (err, data) => err ? onError(err) : onSuccess(data);
+
+//TODO: take this out, put it in a config file.
+var DOWNLOAD_DIR = './js/';
+
+const openHtmlFile = (file: string) => new Promise((resolve: any, reject: any) => {
+    const onResponse = (err: Error, data: string) => err ? onError(err) : onSuccess(data);
     const onError = (error: any) => reject(error);
     const onSuccess = (data: string) => resolve(data);
 
     try {
         fs.readFile(file, 'utf8', onResponse);
-
     } catch (error) {
         onError(error);
     }
 });
 
-const scriptPtrn = /<script.*?src=["']((?:https?:\/\/)?[a-z0-9\/\.\-]+\.[a-z]{2,4}(?![a-z])(?:\/[a-z\/\.\?=0-9\&_]*)?)["']/
+const scriptPtrn = /(<script.*?src=["']((?:https?:)?\/\/[a-z0-9\/\.\-]+\.[a-z]{2,4}(?![a-z])(?:\/[a-z\-\/\.\?=0-9\&_]*)?)["'].*>)/g
 
-const getTag = (html:string) => {
+const getTag = (html: string) => {
     const pattern = RegExp(scriptPtrn);
     const exec = pattern.exec(html);
-    return (exec != null)? exec[1] : null;
+    return (exec != null) ? exec[1] : null;
 }
 
-const parseFile = (file:string) => openHtmlFile(file).then((data) => getTag(data)).catch((e: any) => console.error(e));
+const getAllSrcs = (html: string, idx: number = 0, matches: RegExpExecArray[] = []): RegExpExecArray[] => {
+    const match = RegExp(scriptPtrn).exec(html);
+    if (match == null) return matches;
+    return getAllSrcs(html, match.index, R.append(match, matches));
+}
+
+
+const downloadFile = (exec: RegExpExecArray) => new Promise((resolve: any, reject: any) => {
+    const url = exec[2];
+    const src = url.startsWith("//") ? "http:" + url : url.startsWith("https:") ? url.replace("https:", "http:") : url;
+
+    var file_name = urlParser.parse(src).pathname.split('/').pop();
+    var file = fs.createWriteStream(DOWNLOAD_DIR + file_name);
+
+    try {
+        http.get(src, (res: any) => {
+            res.on('data', function (data: string) {
+                file.write(data);
+            }).on('end', function () {
+                file.end();
+                console.log(file_name + ' downloaded to ' + DOWNLOAD_DIR);
+                resolve(exec);
+            });
+        })
+    } catch (error) {
+        reject(error);
+    }
+});
+
+const downloadFiles = (matches: RegExpExecArray[]) => {
+    const promises = matches.map(downloadFile);
+    Promise.all(promises).then((x) => console.log("all promise:", x)).catch((e) => console.log("all error:", e));
+
+    setTimeout(function () {
+        promises.forEach((p) => console.log(p));
+    }, 1000);
+
+    return matches;
+}
+
+const parseFile = (file: string) => openHtmlFile(file)
+    .then(getAllSrcs)
+    .then(downloadFiles)
+    //.then((html: string) => getTag(html))
+    .catch((e: any) => console.error(e));
 
 exports.openHtmlFile = openHtmlFile;
 exports.getTag = getTag;
 exports.parseFile = parseFile;
+exports.getAllSrcs = getAllSrcs;
 require('make-runnable');
